f9efac859344e661de8eecfa171214e83c7b2e6a
mBarneto <4347791+m-barneto@users.noreply.github.com>
2022-08-29 14:58:55 -0400

    Grabbed the configuration class from the skins plugin, instead of having hardcoded text for the beginning and end text that are prone to error.

--- RustSkinScraper/Program.cs
+++ RustSkinScraper/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
-using System.Net;
 using HtmlAgilityPack;
+using Newtonsoft.Json;
+using RustSkinScraper;
+
 
 // Key is ItemID
 Dictionary<string, List<SkinEntry>> itemSkins = new();
@@ -54,105 +56,11 @@ foreach (var item in itemTable.ChildNodes) {
     idToShortnames[item.ChildNodes[2].InnerHtml] = item.ChildNodes[1].InnerHtml;
 }
 
-// The starting boilerplate of Skins.json
-string startConfig = @"{
-  ""Commands"": [
-    ""skin"",
-    ""skins""
-  ],
-  ""Skins"": [
-";
-// Ending boilerplate of Skins.json
-string endConfig = @"  ],
-  ""Container Panel Name"": ""generic"",
-  ""Container Capacity"": 36,
-  ""UI"": {
-    ""Background Color"": ""0.18 0.28 0.36"",
-    ""Background Anchors"": {
-        ""Anchor Min X"": ""1.0"",
-      ""Anchor Min Y"": ""1.0"",
-      ""Anchor Max X"": ""1.0"",
-      ""Anchor Max Y"": ""1.0""
-    },
-    ""Background Offsets"": {
-    ""Offset Min X"": ""-300"",
-      ""Offset Min Y"": ""-100"",
-      ""Offset Max X"": ""0"",
-      ""Offset Max Y"": ""0""
-    },
-    ""Left Button Text"": ""<size=36><</size>"",
-    ""Left Button Color"": ""0.11 0.51 0.83"",
-    ""Left Button Anchors"": {
-    ""Anchor Min X"": ""0.025"",
-      ""Anchor Min Y"": ""0.05"",
-      ""Anchor Max X"": ""0.325"",
-      ""Anchor Max Y"": ""0.95""
-    },
-    ""Center Button Text"": ""<size=36>Page: {page}</size>"",
-    ""Center Button Color"": ""0.11 0.51 0.83"",
-    ""Center Button Anchors"": {
-    ""Anchor Min X"": ""0.350"",
-      ""Anchor Min Y"": ""0.05"",
-      ""Anchor Max X"": ""0.650"",
-      ""Anchor Max Y"": ""0.95""
-    },
-    ""Right Button Text"": ""<size=36>></size>"",
-    ""Right Button Color"": ""0.11 0.51 0.83"",
-    ""Right Button Anchors"": {
-    ""Anchor Min X"": ""0.675"",
-      ""Anchor Min Y"": ""0.05"",
-      ""Anchor Max X"": ""0.975"",
-      ""Anchor Max Y"": ""0.95""
-    }
-}
-}";
-
-// String to store the formatted data we need
-string skinEntries = "";
-// Current progress counter
-int current = 0;
-
 Stopwatch stopwatch = Stopwatch.StartNew();
 
-// Iterates over each item (not skin)
-for (int i = 0; i < itemSkins.Count; i++) {
-    // If the item doesn't exist in our idToShortnames dict, skip it
-    // This can be true for things such as the twitch rival trophy and other brand new items that don't have a default base variant
-    if (!idToShortnames.ContainsKey(itemSkins.Keys.ToList()[i])) continue;
-    // If the item doesn't have any skins, skip it
-    if (itemSkins[itemSkins.Keys.ToList()[i]].Count == 0) continue;
+string skinsConfig = await SkinsJsonFile.WriteConfigAsync(idToShortnames, itemSkins);
 
-    // Boilerplate for new item entry in Skins.json
-    skinEntries += $"    {{\n      \"Item Shortname\": \"{idToShortnames[itemSkins.Keys.ToList()[i]]}\",\n      \"Permission\": \"\",\n      \"Skins\": [\n        0,\n";
-    // Iterates over each skin entry
-    for (int j = 0; j < itemSkins[itemSkins.Keys.ToList()[i]].Count; j++) {
-        SkinEntry entry = itemSkins[itemSkins.Keys.ToList()[i]][j];
-
-        // Get the skin ID from the entry's link
-        string skinDetails = await client.GetStringAsync(entry.EntryLink);
-        HtmlDocument detailsDOM = new HtmlDocument();
-        detailsDOM.LoadHtml(skinDetails);
-        // Select the ID html element
-        HtmlNode workshopNode = detailsDOM.DocumentNode.SelectSingleNode("/html/body/div[1]/div[2]/div/table/tbody/tr[4]/td[2]/a");
-
-        // If the skin has an ID (only true if it was on the workshop before being added to the game)
-        if (workshopNode != null) {
-            // Add it to our formatted data string
-            skinEntries += $"        {workshopNode.InnerHtml}{(j == itemSkins[itemSkins.Keys.ToList()[i]].Count - 1 ? "" : ",")}\n";
-        } else {
-            // Otherwise just print out which item doesn't have a workshop id
-            Console.WriteLine($"Failed to get workshop ID of {entry}");
-        }
-        // Increment our current progress counter
-        current++;
-    }
-    // End this item's skin entries
-    skinEntries += $"      ]\n    }}{(i == itemSkins.Keys.Count - 1 ? "" : ",")}\n";
-    Console.WriteLine(((float)current / totalSkins).ToString("P"));
-}
-
-// Write our data to a file
-File.WriteAllText("Skins.json", startConfig + skinEntries + endConfig);
+File.WriteAllText("Skins.json", skinsConfig);
 
 Console.WriteLine($"Done! Took {stopwatch.Elapsed.ToString()} to complete.");
 Console.Read();
---
+++ RustSkinScraper/SkinsJsonFile.cs
@@ -0,0 +1,169 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RustSkinScraper {
+    internal class SkinsJsonFile {
+        // https://umod.org/plugins/skins#configuration
+        private class Configuration {
+            [JsonProperty(PropertyName = "Commands")]
+            public string[] Commands = { "skin", "skins" };
+
+            [JsonProperty(PropertyName = "Skins", ObjectCreationHandling = ObjectCreationHandling.Replace)]
+            public List<SkinItem> Skins = new List<SkinItem> { new SkinItem() };
+
+            [JsonIgnore]
+            public Dictionary<string, List<SkinItem>> IndexedSkins = new Dictionary<string, List<SkinItem>>();
+
+            [JsonProperty(PropertyName = "Container Panel Name")]
+            public string Panel = "generic";
+
+            [JsonProperty(PropertyName = "Container Capacity")]
+            public int Capacity = 36;
+
+            [JsonProperty(PropertyName = "UI")]
+            public UIConfiguration UI = new UIConfiguration();
+
+            public class SkinItem {
+                [JsonProperty(PropertyName = "Item Shortname")]
+                // ReSharper disable once MemberCanBePrivate.Local
+                public string Shortname = "shortname";
+
+                [JsonProperty(PropertyName = "Permission")]
+                public string Permission = "";
+
+                [JsonProperty(PropertyName = "Skins", ObjectCreationHandling = ObjectCreationHandling.Replace)]
+                public List<ulong> Skins = new List<ulong> { 0 };
+            }
+
+            public class UIConfiguration {
+                [JsonProperty(PropertyName = "Background Color")]
+                public string BackgroundColor = "0.18 0.28 0.36";
+
+                [JsonProperty(PropertyName = "Background Anchors")]
+                public Anchors BackgroundAnchors = new Anchors { AnchorMinX = "1.0", AnchorMinY = "1.0", AnchorMaxX = "1.0", AnchorMaxY = "1.0" };
+
+                [JsonProperty(PropertyName = "Background Offsets")]
+                public Offsets BackgroundOffsets = new Offsets { OffsetMinX = "-300", OffsetMinY = "-100", OffsetMaxX = "0", OffsetMaxY = "0" };
+
+                [JsonProperty(PropertyName = "Left Button Text")]
+                public string LeftText = "<size=36><</size>";
+
+                [JsonProperty(PropertyName = "Left Button Color")]
+                public string LeftColor = "0.11 0.51 0.83";
+
+                [JsonProperty(PropertyName = "Left Button Anchors")]
+                public Anchors LeftAnchors = new Anchors { AnchorMinX = "0.025", AnchorMinY = "0.05", AnchorMaxX = "0.325", AnchorMaxY = "0.95" };
+
+                [JsonProperty(PropertyName = "Center Button Text")]
+                public string CenterText = "<size=36>Page: {page}</size>";
+
+                [JsonProperty(PropertyName = "Center Button Color")]
+                public string CenterColor = "0.11 0.51 0.83";
+
+                [JsonProperty(PropertyName = "Center Button Anchors")]
+                public Anchors CenterAnchors = new Anchors { AnchorMinX = "0.350", AnchorMinY = "0.05", AnchorMaxX = "0.650", AnchorMaxY = "0.95" };
+
+                [JsonProperty(PropertyName = "Right Button Text")]
+                public string RightText = "<size=36>></size>";
+
+                [JsonProperty(PropertyName = "Right Button Color")]
+                public string RightColor = "0.11 0.51 0.83";
+
+                [JsonProperty(PropertyName = "Right Button Anchors")]
+                public Anchors RightAnchors = new Anchors { AnchorMinX = "0.675", AnchorMinY = "0.05", AnchorMaxX = "0.975", AnchorMaxY = "0.95" };
+
+                [JsonIgnore]
+                public string ParsedUI;
+
+                [JsonIgnore]
+                public int IndexPagePrevious, IndexPageCurrent, IndexPageNext;
+
+                public class Anchors {
+                    [JsonProperty(PropertyName = "Anchor Min X")]
+                    public string AnchorMinX = "0.0";
+
+                    [JsonProperty(PropertyName = "Anchor Min Y")]
+                    public string AnchorMinY = "0.0";
+
+                    [JsonProperty(PropertyName = "Anchor Max X")]
+                    public string AnchorMaxX = "1.0";
+
+                    [JsonProperty(PropertyName = "Anchor Max Y")]
+                    public string AnchorMaxY = "1.0";
+
+                    [JsonIgnore]
+                    public string AnchorMin => $"{AnchorMinX} {AnchorMinY}";
+
+                    [JsonIgnore]
+                    public string AnchorMax => $"{AnchorMaxX} {AnchorMaxY}";
+                }
+
+                public class Offsets {
+                    [JsonProperty(PropertyName = "Offset Min X")]
+                    public string OffsetMinX = "0";
+
+                    [JsonProperty(PropertyName = "Offset Min Y")]
+                    public string OffsetMinY = "0";
+
+                    [JsonProperty(PropertyName = "Offset Max X")]
+                    public string OffsetMaxX = "100";
+
+                    [JsonProperty(PropertyName = "Offset Max Y")]
+                    public string OffsetMaxY = "100";
+
+                    [JsonIgnore]
+                    public string OffsetMin => $"{OffsetMinX} {OffsetMinY}";
+
+                    [JsonIgnore]
+                    public string OffsetMax => $"{OffsetMaxX} {OffsetMaxY}";
+                }
+            }
+        }
+
+        public static async Task<string> WriteConfigAsync(Dictionary<string, string> idToShortnames, Dictionary<string, List<SkinEntry>> itemSkins) {
+            Configuration cfg = new Configuration();
+            HttpClient client = new HttpClient();
+
+            // Iterates over each item (not skin)
+            for (int i = 0; i < itemSkins.Count; i++) {
+                // If the item doesn't exist in our idToShortnames dict, skip it
+                // This can be true for things such as the twitch rival trophy and other brand new items that don't have a default base variant
+                if (!idToShortnames.ContainsKey(itemSkins.Keys.ToList()[i])) continue;
+                // If the item doesn't have any skins, skip it
+                if (itemSkins[itemSkins.Keys.ToList()[i]].Count == 0) continue;
+
+                // Create a SkinItem for the item
+                Configuration.SkinItem skinItem = new Configuration.SkinItem();
+                skinItem.Shortname = idToShortnames[itemSkins.Keys.ToList()[i]];
+
+                // Iterates over each skin entry
+                for (int j = 0; j < itemSkins[itemSkins.Keys.ToList()[i]].Count; j++) {
+                    SkinEntry entry = itemSkins[itemSkins.Keys.ToList()[i]][j];
+
+                    // Get the skin ID from the entry's link
+                    string skinDetails = await client.GetStringAsync(entry.EntryLink);
+                    HtmlDocument detailsDOM = new HtmlDocument();
+                    detailsDOM.LoadHtml(skinDetails);
+                    // Select the ID html element
+                    HtmlNode workshopNode = detailsDOM.DocumentNode.SelectSingleNode("/html/body/div[1]/div[2]/div/table/tbody/tr[4]/td[2]/a");
+
+                    // If the skin has an ID (only true if it was on the workshop before being added to the game)
+                    if (workshopNode != null) {
+                        // Add it to our formatted data string
+                        skinItem.Skins.Add(ulong.Parse(workshopNode.InnerHtml));
+                    } else {
+                        // Otherwise just print out which item doesn't have a workshop id
+                        Console.WriteLine($"Failed to get workshop ID of {entry}");
+                    }
+                }
+            }
+
+            return JsonConvert.SerializeObject(cfg, Formatting.Indented);
+        }
+    }
+}
